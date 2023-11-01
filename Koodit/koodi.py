2569c8bda91aa876f4214c1fc8aaa669ec244d63
JunnuD <junnudanhammer@gmail.com>
2023-11-01 11:26:38 +0200

    Update koodi.py
    Funktiot lisätty ja toiminnallisuus korostettu

--- Koodit/koodi.py
+++ Koodit/koodi.py
@@ -1,30 +1,45 @@
 from lista import my_list
 
-# USE FUNCTIONS !!!!!
+# Function to add data to the list
+def add_data():
+    tieto = input('Syötä tieto: ')
+    my_list.append(tieto)
+    print('Tieto lisätty listaan!')
 
+# Function to print the list
+def print_list():
+    print('Listan sisältö:')
+    for tieto in my_list:
+        print(tieto)
 
+def print_item_count():
+    count = len(my_list)
+    print(f'Listassa on {count} kpl tietoja ')
 
 while True:
+    print()
     print('Valitse toiminto: ')
     print('1. Lisää listaan tietoa ')
     print('2. Tulosta lista ')
-    print('3. Lopeta ohjelma')
+    print('3. Tulosta listan pituus')
+    print('4. Lopeta ohjelma')
+    print()
 
     valinta = input('Syötä valintasi: ')
 
     if valinta == '1':
-        tieto = input('Syötä tieto: ')
-        my_list.append(tieto)
-        print('Tieto lisätty listaan! ')
+        add_data()
 
     elif valinta == '2':
-        print('Listan sisältö: ')
-        for tieto in my_list:
-            print(tieto)
+        print_list()
 
     elif valinta == '3':
+        print_item_count()
+
+    elif valinta == '4':
         print('Ohjelma päättyy. ')
+        print()
         break
 
-    else: 
+    else:
         print('Virheellinen valinta. Yritä uudelleen')
